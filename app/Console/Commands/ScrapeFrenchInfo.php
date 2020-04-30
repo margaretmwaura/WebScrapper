746744cac9b22a26b90c27281d72dd36918356b7
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-04-30 09:54:41 +0300

    saving the letters in an array

--- app/Console/Commands/ScrapeFrenchInfo.php
+++ app/Console/Commands/ScrapeFrenchInfo.php
@@ -42,53 +42,75 @@ class ScrapeFrenchInfo extends Command
      */
     public function handle()
     {
-
         $client = new Client();
+
         $crawler = $client->request('GET', 'https://www.rocketlanguages.com/french/lessons/french-alphabet');
+
         $html = file_get_contents('https://www.rocketlanguages.com/french/lessons/french-alphabet');
+
         $doc = new DOMDocument();
+
         @$doc->loadHTML($html);
+
         $tags = $doc->getElementsByTagName('audio');
+
         $description_array = [];
-        $letters_array = $crawler->filter('.output')->each(function ($node) {
+
+        $letters_array = [];
+
+        $letters = $crawler->filter('.output')->each(function ($node) {
+
             return $letters[] = $node->text();
+
         });
-        dd($letters_array);
-        $crawler->filter('.output')->each(function ($node) use ($letters_array,$description_array){
-            $content = $node->text();
-            $word = explode(" " , $content);
-            echo $word[0]."\n";
-            echo $word[1]."\n";
-            array_push($letters_array,$word[0]);
-            print_r($letters_array);
-            array_push($description_array,$word[1]);
-            print_r($description_array);
-            $letters_array[] = $word[0];
-            $description_array[] = $word[1];
-        });
+        foreach($letters as $letter){
+
+            $single_letter = explode(" " , $letter);
+
+            array_push($letters_array,$single_letter[0]);
+
+            array_push($description_array,$single_letter[1]);
+        }
         $filenames = [];
+
         foreach ($tags as $tag) {
+
             $url = $tag->getAttribute('src');
+
             echo $tag->getAttribute('src')."\n";
+
             $urlParts = explode("/", $url);
+
             print $urlParts[6]."\n";
+
             $ch = curl_init();
+
             $timeout = 5;
+
             curl_setopt($ch, CURLOPT_URL, $url);
+
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
             curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
             $data = curl_exec($ch);
+
             Storage::disk('local')->put($urlParts[6], $data);
+
             curl_close($ch);
+
             array_push($filenames,$urlParts[6]);
         }
         for ($i = 0 ; $i < count($filenames) ; $i++)
         {
             $vowel = new Vowel();
-            dd($letters_array);
+
             $vowel->name = $letters_array[$i];
+
             $vowel->description = $description_array[$i];
+
             $vowel->filename = $filenames[$i];
+
             $vowel->save();
 
         }
